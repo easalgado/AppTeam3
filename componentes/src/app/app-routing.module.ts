b7070ab822c093e9652ef044e028e074daadd713
easalgado <202100446@umh.edu.hn>
2022-03-20 21:00:30 -0600

    creacion_pages

--- componentes/src/app/app-routing.module.ts
+++ componentes/src/app/app-routing.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+ 
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'contactos',
+    loadChildren: () => import('./pages/contactos/contactos.module').then( m => m.ContactosPageModule)
+  },
+  {
+    path: 'descripcion',
+    loadChildren: () => import('./pages/descripcion/descripcion.module').then( m => m.DescripcionPageModule)
+  },
+  {
+    path: 'tabla-trabajo',
+    loadChildren: () => import('./pages/tabla-trabajo/tabla-trabajo.module').then( m => m.TablaTrabajoPageModule)
   },
 ];
 
