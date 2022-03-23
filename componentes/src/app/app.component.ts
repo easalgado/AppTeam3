2d9269a9a2936d9bc60058484361d6a2ae8b571f
Juan13vasquez <100739473+Juan13vasquez@users.noreply.github.com>
2022-03-22 18:42:05 -0600

    Update app.component.ts
    carrusel_2

--- componentes/src/app/app.component.ts
+++ componentes/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class HomePage {
+
+  slides = [
+    {
+     img: 'assets/img/M1.jpg',
+     titulo: 'MATRICULATE YA <br>in UMH'
+    },
+    {
+     img: 'assets/img/UMH1.JPG',
+     titulo: 'SOMOS<br>UMH'
+    },
+    {
+     img: 'assets/img/M1.jpg',
+     titulo: 'UMH<br>TU INIVERSIDAD <BR> DE EXITO'
+    },
+   ]
+
 }
